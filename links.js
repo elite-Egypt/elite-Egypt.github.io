ff731f115aa98781c4492eb31fd65977047ab380
elite-Egypt <qxa346385@gmail.com>
2026-03-29 12:56:51 +0200

    Update links.js

--- links.js
+++ links.js
@@ -1,13 +1,9 @@
-// مصفوفة الزبائن - ضيف سطر جديد لكل زبون هنا
+// قائمة زبائن صفحة النخبة - ضيف سطر الزبون بين القوسين [ ]
 const clients = [
-    // مثال لزبون VIP (واخد أول مربعين فوق يمين)
-    { coords: "0,0,216,216", url: "https://facebook.com/VIP_LINK", title: "VIP Client" },
-    
-    // مثال لزبون عادي (المربع الثالث في الصف الأول)
-    { coords: "216,0,324,108", url: "https://facebook.com/NORMAL_LINK", title: "Normal Client" }
+
 ];
 
-// المحرك اللي بيشغل الروابط (سيبه زي ما هو)
+// المحرك الذكي - ملمسوش نهائي
 const mapLinks = document.getElementById('map-links');
 if (mapLinks) {
     clients.forEach(client => {
@@ -20,4 +16,3 @@ if (mapLinks) {
         mapLinks.appendChild(area);
     });
 }
-
